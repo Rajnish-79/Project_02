d939a7c96b8a79856fc80c1f608fe1aecbf7fc32
Rajnish79 <pathakrajnish43@gmail.com>
2025-05-08 20:22:24 +0530

    set method_overrite

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 const ejsMate =require("ejs-mate")
 
 
@@ -16,7 +17,7 @@ main().then(() =>{
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended: true}));
-// app.use(methodOverride("_method"));
+app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
